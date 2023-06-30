e0d5b91ac083e2a71cf9793864dcf7bb4a361f5c
MajorPickaxe <taylan.ozdemir505@gmail.com>
2023-06-30 15:52:04 +1000

    Reckon Code Challenge, w/ Bonus Req

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {React, useState, useEffect} from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  //Increment Function
+  const increment = () => {
+    setCounter(count => count + 1);
+  };
+
+  //Auto-Increments the Counter by 1, every 2 seconds.
+  useEffect(() => {
+    console.log("Running")
+    const interval = setInterval(() => {
+      setCounter(count => count + 1)
+    }, 2000)
+    return () => 
+     clearInterval(interval);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Counter: {counter}</h1>
+        <button
+          className="Button"
+          onClick={increment}>
+            Increment
+        </button>
       </header>
     </div>
   );
